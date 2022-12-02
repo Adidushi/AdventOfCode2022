3a464feb047d7a28d05d842adbe6fec4aafa9b3e
Adi Bar Ilan <adibarilan@gmail.com>
2022-12-02 13:29:59 +0200

    Add day 2

--- day 02/solution.py
+++ day 02/solution.py
@@ -1,13 +1,60 @@
+
+
 def q1():
     with open('day 02\input.txt', 'r') as f:
         input = f.read().splitlines()
-    
+
+    winning = (('A', 'Y'), ('B', 'Z'), ('C', 'X'))
+    tie = (('A', 'X'), ('B', 'Y'), ('C', 'Z'))
+
+    pointage = {
+        'X': 1,
+        'Y': 2,
+        'Z': 3
+    }
+
+    total_score = 0
+
+    for game in input:
+        moves = tuple(game.split())
+        if moves in winning:
+            total_score += 6
+        elif moves in tie:
+            total_score += 3
+
+        them, me = moves
+        total_score += pointage[me]
+
+    return total_score
+
+
 def q2():
     with open('day 02\input.txt', 'r') as f:
         input = f.read().splitlines()
 
+    decision_tree = {
+        'Z': {
+            'A': 2+6,
+            'B': 3+6,
+            'C': 1+6
+        },
+        'Y': {
+            'A': 1+3,
+            'B': 2+3,
+            'C': 3+3
+        },
+        'X': {
+            'A': 3,
+            'B': 1,
+            'C': 2
+        }
+    }
+
+    return sum(decision_tree[g.split()[1]][g.split()[0]] for g in input)
+
+
 if __name__ == '__main__':
-    from time import perf_counter as  pc
+    from time import perf_counter as pc
     st = pc()
     print(f'Part 1: {q1()}')
     pt1 = pc()
