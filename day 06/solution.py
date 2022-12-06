d1a378779cf689dd9d5e5d9f1890951ec3ae0a56
Adi Bar Ilan <adiba@radware.com>
2022-12-06 07:08:51 +0200

    Add day 6

--- day 06/solution.py
+++ day 06/solution.py
@@ -1,10 +1,27 @@
+def is_unique(s: str) -> bool:
+    return len(set(s)) == len(s)
+
 def q1():
     with open('day 06\input.txt', 'r') as f:
-        input = f.read().splitlines()
+        input = f.read()
+    
+    counter = 4
+    while not is_unique(input[:4]):
+        counter += 1
+        input = input[1:]
+
+    return counter
     
 def q2():
     with open('day 06\input.txt', 'r') as f:
-        input = f.read().splitlines()
+        input = f.read()
+
+    counter = 14
+    while not is_unique(input[:14]):
+        counter += 1
+        input = input[1:]
+
+    return counter
 
 if __name__ == '__main__':
     from time import perf_counter as  pc
